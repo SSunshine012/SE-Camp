a4bd29e681fa9e2538beb91432293f3424d5f31e
SSunshine012 <152692536+SSunshine012@users.noreply.github.com>
2023-12-26 19:10:47 +0700

    Almost finish

--- index.php
+++ index.php
@@ -6,23 +6,34 @@
 
 <body>
     <?php $multi_x = 2; ?>
+    <br><br>
     <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
-
-    <table>
-        <tbody>
-            <tr>
-                <td></td>
-                <td>x</td>
-                <td></td>
-                <td>=</td>
-                <td></td>
-            </tr>
-        </tbody>
-    </table>
+    <br><br><br><br>
+    <div class="box"> 
+        <?php
+        for($i=1;$i<=24;$i++){
+            echo $multi_x ." x ". $i. " = ". $multi_x*$i."<br>";
+        }
+        ?>
+    </div>
     <style>
-        table {
-            width: 150px;
-            height: 265px;
+        *{
+            padding: 0;
+            margin: 0;
+            box-sizing: border-box;
+        }
+        body{
+            width: 100%;
+            height: 90vh;
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+        }
+        .box{
+            width: 300px;
+            height: 400px;
+            overflow-y: scroll;
+            font-size: 30px;
         }
     </style>
 </body>
